a6235300294844999b85aa4860338b5fc8756fd6
redthing1 <redthing1@alt.icu>
2023-04-16 17:19:54 -0700

    redbuild support

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# dlang wasm testing
+
+## build
+
++ install [redbuild](https://github.com/redthing1/redbuild2)
+
+```sh
+redbuild build
+```
---
+++ build.sh
@@ -0,0 +1,2 @@
+pushd test1 && dub build --compiler=ldc2 --arch=wasm32-unknown-unknown-wasm && popd
+pushd test2 && dub build --compiler=ldc2 --arch=wasm32-unknown-unknown-wasm && popd
